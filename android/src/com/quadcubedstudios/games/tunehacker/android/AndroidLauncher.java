1bae5e2a3a83f58ff051e5f8b64980aaa1c8c26b
Technohacker <technohacker1995@gmail.com>
2015-06-04 18:25:34 +0530

    Changed Package name and made Splash Screen

--- android/src/com/quadcubedstudios/games/tunehacker/android/AndroidLauncher.java
+++ android/src/com/quadcubedstudios/games/tunehacker/android/AndroidLauncher.java
@@ -1,10 +1,10 @@
-package com.squarecubedstudios.games.tunehacker.android;
+package com.quadcubedstudios.games.tunehacker.android;
 
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.squarecubedstudios.games.tunehacker.TuneHacker;
+import com.quadcubedstudios.games.tunehacker.TuneHacker;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
